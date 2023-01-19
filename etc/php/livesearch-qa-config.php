5b7cb82d5a01d57cfcbb3e4f197510edd6f7ccf2
ruslankostiv <rkostiv@adobe.com>
2023-01-19 15:51:06 -0600

    Magento 2.4.6 Compatibility added

--- etc/php/livesearch-qa-config.php
+++ etc/php/livesearch-qa-config.php
@@ -8,10 +8,6 @@ return [
         'magento-storefront-events-sdk@^1/dist/index' => 'magento-storefront-events-sdk@qa/dist/index',
         'magento-storefront-event-collector@^1/dist/index' => 'magento-storefront-event-collector@qa/dist/index',
     ],
-    'services-id/ServicesId/etc/csp_whitelist.xml' => [
-        'api.magento.com' => 'qa-api.magedevteam.com',
-        'commerce.adobe.io' => 'commerce-int.adobe.io',
-    ],
     'data-services/DataServices/etc/csp_whitelist.xml' => [
         'commerce.adobedtm.com' => 'js.magento-datasolutions.com',
         'commerce.adobedc.net' => 'com-magento-qa1.collector.snplow.net',
